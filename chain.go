288bf6d874a3f792cd3547520dd4693a714b449a
Steven Spencer <steve@bigfrog.net>
2025-06-21 00:33:12 -0700

    lots of changes

--- chain.go
+++ chain.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"math"
-
-	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
@@ -27,7 +25,6 @@ func NewChain(origin Vector, jointCount int, linkSize int, angleConstraint float
 	c.angles = append(c.angles, 0)
 
 	for i := 1; i < jointCount; i++ {
-		//joints.add(PVector.add(joints.get(i - 1), new PVector(0, this.linkSize)));
 		c.joints = append(c.joints, c.joints[i-1].Add(NewVector(0, float64(linkSize))))
 		c.angles = append(c.angles, 0)
 	}
@@ -45,57 +42,42 @@ func (c *Chain) Resolve(pos Vector) {
 	c.angles[0] = pos.Subtract(c.joints[0]).Angle()
 
 	for i := 1; i < len(c.joints); i++ {
-		//	float curAngle = PVector.sub(joints.get(i - 1), joints.get(i)).heading();
 		curAngle := c.joints[i-1].Subtract(c.joints[i]).Angle()
-		//	angles.set(i, constrainAngle(curAngle, angles.get(i - 1), angleConstraint));
 		c.angles[i] = constrainAngle(curAngle, c.angles[i-1], c.angleConstraint)
-		//	joints.set(i, PVector.sub(joints.get(i - 1), PVector.fromAngle(angles.get(i)).setMag(linkSize)));
 		c.joints[i] = c.joints[i-1].Subtract(FromAngle(c.angles[i]).SetMag(float64(c.linkSize)))
 	}
 }
 
-func (c *Chain) Draw(color rl.Color, bodyFactor float32) {
-	const (
-		//innerRadius   = 16
-		//outerRadius   = 24
-		lineThickness = 6
-	)
-	for i := 0; i < len(c.joints)-1; i++ {
-		startJoint := c.joints[i]
-		endJoint := c.joints[i+1]
-
-		rl.DrawLineEx(vec2(startJoint), vec2(endJoint), lineThickness, color)
-	}
-
-	color.A = 128
-	for i, joint := range c.joints {
-		b := bodyWidth(i) / bodyFactor
-		//rl.DrawRing(vec2(joint), b-10, b, 0, 360, 72, color)
-		rl.DrawCircle(int32(joint.X), int32(joint.Y), b, color)
-	}
-
-	joint := c.joints[0]
-	b := bodyWidth(0) / bodyFactor
-	color.A = 255
-	rl.DrawCircle(int32(joint.X), int32(joint.Y), b, color)
-
-}
-
-func bodyWidth(i int) float32 {
-	switch i {
-	case 0:
-		return 74
-	case 1:
-		return 80
-	default:
-		return float32(64 - i)
+func (c *Chain) DeleteJoint() {
+	if len(c.joints) > 3 {
+		c.joints = c.joints[:len(c.joints)-1]
+		c.angles = c.angles[:len(c.angles)-1]
+		c.Resolve(c.joints[0])
 	}
 }
 
-func vec2(v Vector) rl.Vector2 {
-	return rl.Vector2{X: float32(v.X), Y: float32(v.Y)}
-}
+func (c *Chain) AddJoint() {
+	lastJoint := c.joints[len(c.joints)-1]
+	var newJoint Vector
 
-func vec(p rl.Vector2) Vector {
-	return Vector{X: float64(p.X), Y: float64(p.Y)}
+	if len(c.joints) == 1 {
+		// If there's only one joint, add a new joint directly below it
+		newJoint = lastJoint.Add(NewVector(0, float64(c.linkSize)))
+	} else {
+		// Calculate direction from second last to last joint
+		secondLastJoint := c.joints[len(c.joints)-2]
+		direction := lastJoint.Subtract(secondLastJoint).Normalize()
+
+		// Add new joint in the same direction
+		newJoint = lastJoint.Add(direction.SetMag(float64(c.linkSize)))
+	}
+
+	c.joints = append(c.joints, newJoint)
+
+	// Add a new angle (use the same angle as the last joint)
+	lastAngle := c.angles[len(c.angles)-1]
+	c.angles = append(c.angles, lastAngle)
+
+	// Resolve the chain to ensure proper positioning
+	c.Resolve(c.joints[0])
 }
--- main.go
+++ main.go
@@ -1,180 +1,470 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"math/rand/v2"
+	"strings"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-	windowWidth  = 1600
-	windowHeight = 1200
-	border       = 0 // don't adjust near the border
-	minSpeed     = 150
-	minAdjTime   = 0.8
+	ScreenWidth  = 1600
+	ScreenHeight = 1200
+	MinSpeed     = 100
+	MaxSpeed     = 200
+	NumSnakes    = 14
+
+	CollisionTime = 1.5
 )
+
+type Snake struct {
+	name           string
+	pos            rl.Vector2
+	vel            rl.Vector2
+	chain          *Chain
+	color          rl.Color
+	bodyFactor     float32
+	radius         float32
+	collisionTime  float64
+	collisionColor rl.Color
+}
+
+type Food struct {
+	pos    rl.Vector2
+	radius float32
+}
 
 var (
 	background = rl.NewColor(43, 60, 80, 255)
+	snakes     []*Snake
+	food       Food
 )
 
 func main() {
 	rl.SetConfigFlags(rl.FlagVsyncHint)
 
-	rl.InitWindow(windowWidth, windowHeight, "Procedural Animation")
+	rl.InitWindow(ScreenWidth, ScreenHeight, "Snakes")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
-	// lastTime := rl.GetTime()
-	snakes := []*snake{
-		newSnake("orange", rl.Orange),
-		newSnake("yellow", rl.Yellow),
-		newSnake("purple", rl.Purple),
-		newSnake("blue", rl.Blue),
-		newSnake("red", rl.Red),
-		newSnake("green", rl.Green),
-		newSnake("pink", rl.Pink),
-		newSnake("sky blue", rl.SkyBlue),
-		//newSnake("maroon", rl.Maroon),
-		newSnake("violet", rl.Violet),
-		newSnake("magenta", rl.Magenta),
-		newSnake("gold", rl.Gold),
-	}
+	initSnakes()
+	initFood()
 
+	pause := false
 	for !rl.WindowShouldClose() {
-		dt := rl.GetFrameTime()
 
-		for _, s := range snakes {
-			s.move(dt)
+		if !pause {
+			dt := rl.GetFrameTime()
+			update(dt)
+			checkPicnic()
+			checkCollisions()
 		}
 
 		rl.BeginDrawing()
 		rl.ClearBackground(background)
-		for _, s := range snakes {
-			s.draw()
-		}
+
+		drawFood()
+		drawSnakes()
+		status()
 
 		rl.DrawFPS(10, 10)
 
 		rl.EndDrawing()
-	}
-}
 
-type snake struct {
-	name       string
-	pos        Vector
-	dir        Vector
-	chain      *Chain
-	speed      float64
-	color      rl.Color
-	bodyFactor float32
-	adjTime    float64
-	adjPeriod  float64
-}
-
-func newSnake(name string, color rl.Color) *snake {
-
-	pos := NewVector(rand.Float64()*windowWidth, rand.Float64()*windowHeight)
-	dir := NewVector(randDir(), randDir())
-
-	c := NewChain(pos, rand.IntN(24)+8, rand.IntN(24)+16, math.Pi/((rand.Float64()*4)+4))
-	c.Resolve(pos)
-
-	snake := &snake{
-		name:       name,
-		pos:        pos,
-		dir:        dir,
-		chain:      c,
-		color:      color,
-		speed:      float64(rand.IntN(minSpeed) + minSpeed),
-		bodyFactor: rand.Float32()*4.0 + 2,
-		adjPeriod:  rand.Float64()*minAdjTime + minAdjTime,
-	}
-
-	return snake
-}
-
-func (s *snake) collision(snakes []*snake, index int, dt float32) bool {
-	radius := bodyWidth(0) / s.bodyFactor / 2
-
-	for i, other := range snakes {
-		if index == i {
-			continue
+		if rl.IsKeyPressed(rl.KeyR) {
+			initSnakes()
+			initFood()
 		}
-		otherRadius := bodyWidth(0) / other.bodyFactor / 2
-		if rl.CheckCollisionCircles(vec2(s.pos), radius, vec2(other.pos), otherRadius) {
-			s.dir.X = -s.dir.X * 2.0
-			s.dir.Y = -s.dir.Y * 2.0
-			s.pos.X += s.dir.X * s.speed * float64(dt)
-			s.pos.Y += s.dir.Y * s.speed * float64(dt)
 
-			return true
+		if rl.IsKeyPressed(rl.KeyP) || rl.IsKeyPressed(rl.KeySpace) {
+			pause = !pause
 		}
 	}
-
-	return false
 }
 
-func (s *snake) move(dt float32) {
-	s.pos.X += s.dir.X * s.speed * float64(dt)
-	s.pos.Y += s.dir.Y * s.speed * float64(dt)
+func status() {
+	statusJoints(ScreenHeight - 55)
+	statusLinkSize(ScreenHeight - 25)
+}
 
-	curTime := rl.GetTime()
-	if s.pos.X < 0 || s.pos.X > windowWidth {
-		s.adjTime = curTime
-		s.dir.X = -s.dir.X
-	}
-	if s.pos.Y < 0 || s.pos.Y > windowHeight {
-		s.adjTime = curTime
-		s.dir.Y = -s.dir.Y
+func statusLinkSize(y int32) {
+	sb := strings.Builder{}
+
+	sb.WriteString("Factor:  ")
+	for _, s := range snakes {
+		sb.WriteString(fmt.Sprintf("%s: %3d  ", s.name, int32(s.bodyFactor*100)))
 	}
 
-	if curTime-s.adjTime > s.adjPeriod {
-		//old := s.dir
-		changeDir := rand.Float64() < 0.3
-		if s.dir.X > s.dir.Y {
-			s.dir.X = adjustDir(s.dir.X, 0.1)
-			if changeDir {
-				s.dir.X = -s.dir.X
+	rl.DrawText(sb.String(), 10, y, 20, rl.White)
+}
+
+func statusJoints(y int32) {
+	sb := strings.Builder{}
+
+	sb.WriteString("Joints:  ")
+	for _, s := range snakes {
+		sb.WriteString(fmt.Sprintf("%s: %3d  ", s.name, len(s.chain.joints)))
+	}
+
+	rl.DrawText(sb.String(), 10, y, 20, rl.White)
+}
+
+func initFood() {
+	radius := rand.Float32()*30 + 10
+	var border float32 = 100.0
+	pos := rl.Vector2{
+		X: border + (rand.Float32()*ScreenWidth - 2*border),
+		Y: border + (rand.Float32()*ScreenHeight - 2*border),
+	}
+
+	food.radius = radius
+	food.pos = pos
+}
+
+func initSnakes() {
+	snakes = make([]*Snake, NumSnakes)
+
+	for i := 0; i < NumSnakes; i++ {
+		factor := rand.Float32()*0.4 + 0.15
+		radius := bodyWidth(0, factor)
+		speed := MinSpeed + rand.Float64()*(MaxSpeed-MinSpeed)
+		angle := rand.Float64() * math.Pi * 2
+
+		pos := rl.Vector2{
+			X: radius + (rand.Float32()*ScreenWidth - 2*radius),
+			Y: radius + (rand.Float32()*ScreenHeight - 2*radius),
+		}
+
+		vel := rl.Vector2{
+			X: float32(math.Cos(angle) * speed),
+			Y: float32(math.Sin(angle) * speed),
+		}
+
+		v := Vector{
+			X: float64(pos.X),
+			Y: float64(pos.Y),
+		}
+		chain := NewChain(v, rand.IntN(18)+12, rand.IntN(24)+12, math.Pi/((rand.Float64()*4)+4))
+		chain.Resolve(v)
+		snake := Snake{
+			name:       fmt.Sprintf("%d", i),
+			chain:      chain,
+			pos:        pos,
+			vel:        vel,
+			radius:     radius,
+			bodyFactor: factor,
+			color:      randomColor(),
+		}
+
+		snakes[i] = &snake
+
+	}
+}
+
+func update(dt float32) {
+	for _, s := range snakes {
+		// Update position
+		//fmt.Printf("from: %v ", s.pos)
+		s.pos.X += s.vel.X * dt
+		s.pos.Y += s.vel.Y * dt
+		//fmt.Printf("to: %v\n", s.pos)
+
+		// Boundary collision detection and response
+		if s.pos.X-s.radius <= 0 {
+			s.pos.X = s.radius
+			s.vel.X = -s.vel.X
+		} else if s.pos.X+s.radius >= ScreenWidth {
+			s.pos.X = ScreenWidth - s.radius
+			s.vel.X = -s.vel.X
+		}
+
+		if s.pos.Y-s.radius <= 0 {
+			s.pos.Y = s.radius
+			s.vel.Y = -s.vel.Y
+		} else if s.pos.Y+s.radius >= ScreenHeight {
+			s.pos.Y = ScreenHeight - s.radius
+			s.vel.Y = -s.vel.Y
+		}
+
+		s.vel.X = clamp(s.vel.X, MinSpeed, MaxSpeed)
+		s.vel.Y = clamp(s.vel.Y, MinSpeed, MaxSpeed)
+		s.chain.Resolve(vec(s.pos))
+
+	}
+}
+
+func clamp(v, min, max float32) float32 {
+	var f float32 = 1.0
+	if v < 0 {
+		v = -v
+		f = -1.0
+	}
+
+	if v < min {
+		return min * f
+	} else if v > max {
+		return max * f
+	}
+
+	return v * f
+}
+
+func checkPicnic() {
+	for i := 0; i < len(snakes); i++ {
+		s1 := snakes[i]
+		pos1 := vec2(s1.chain.joints[0])
+		pos2 := food.pos
+
+		// Calculate distance between centers
+		dx := pos2.X - pos1.X
+		dy := pos2.Y - pos1.Y
+		distance := float32(math.Sqrt(float64(dx*dx) + float64(dy*dy)))
+		minDistance := s1.radius + food.radius
+		if distance < minDistance && distance > 0 {
+			s1.collisionColor = rl.Gold
+			s1.chain.AddJoint()
+			s1.collisionTime = rl.GetTime()
+			sqrt := math.Sqrt(float64(food.radius))
+			s1.bodyFactor += float32(math.Sqrt(float64(food.radius))) / 200.0
+
+			s1.bodyFactor += float32(sqrt / 200.0)
+			n := int(math.Round(sqrt))
+			for j := 0; j < n; j++ {
+				s1.chain.AddJoint()
 			}
-		} else {
-			s.dir.Y = adjustDir(s.dir.Y, 0.1)
-			if changeDir {
-				s.dir.Y = -s.dir.Y
+			s1.chain.linkSize++
+			fmt.Printf("Ate food r=%0.2f, sqrt=%0.2f, f2=%0.2f\n", food.radius, sqrt, sqrt/200.0)
+			initFood()
+		}
+	}
+}
+
+func checkCollisions() {
+	deleteId := -1
+	for i, s := range snakes {
+		if len(s.chain.joints) < 6 || s.bodyFactor < 0.12 {
+			fmt.Printf("Deleting %s, f=%0.2f, joints=%d\n", s.name, s.bodyFactor, len(s.chain.joints))
+			deleteId = i
+		}
+	}
+
+	if deleteId >= 0 {
+		snakes = append(snakes[:deleteId], snakes[deleteId+1:]...)
+	}
+
+	collisionAddColor := rl.NewColor(0, 255, 0, 153)
+	collisionDeleteColor := rl.NewColor(255, 0, 0, 153)
+
+	// Check collisions between all pairs of snakes
+	for i := 0; i < len(snakes); i++ {
+		for j := i + 1; j < len(snakes); j++ {
+			s1 := snakes[i]
+			s2 := snakes[j]
+
+			pos1 := vec2(s1.chain.joints[0])
+			pos2 := vec2(s2.chain.joints[0])
+
+			// Calculate distance between centers
+			dx := pos2.X - pos1.X
+			dy := pos2.Y - pos1.Y
+			distance := float32(math.Sqrt(float64(dx*dx) + float64(dy*dy)))
+
+			// Check if collision occurred
+			minDistance := s1.radius + s2.radius
+			if distance < minDistance && distance > 0 {
+				// Collision detected - resolve it
+
+				t := rl.GetTime()
+				if t-s1.collisionTime > CollisionTime && t-s2.collisionTime > CollisionTime {
+					s1.collisionTime = t
+					s2.collisionTime = t
+					//m1 := math.Sqrt(float64(s1.vel.X*s1.vel.X) + float64(s1.vel.Y*s1.vel.Y))
+					//m2 := math.Sqrt(float64(s2.vel.X*s2.vel.X) + float64(s2.vel.Y*s2.vel.Y))
+					var winner, loser *Snake
+					if s1.bodyFactor > s2.bodyFactor {
+						winner = s1
+						loser = s2
+					} else {
+						winner = s2
+						loser = s1
+					}
+
+					winner.bodyFactor += 0.01
+					winner.chain.AddJoint()
+					winner.collisionColor = collisionAddColor
+
+					loser.bodyFactor -= 0.01
+					loser.chain.DeleteJoint()
+					loser.collisionColor = collisionDeleteColor
+				}
+
+				resolveCollisionWithMass(s1, s2, dx, dy, distance)
+				s1.chain.Resolve(vec(s1.pos))
+				s1.chain.Resolve(vec(s2.pos))
+
 			}
 		}
+	}
+}
 
-		//fmt.Printf("adjusting %s after %0.2f seconds from %s to %s\n", s.name, curTime-s.adjTime, old, s.dir)
-		s.adjTime = curTime
+func vec2(v Vector) rl.Vector2 {
+	return rl.Vector2{X: float32(v.X), Y: float32(v.Y)}
+}
 
-		//s.speed = float64(rl.Clamp(float32(s.speed+float64(rand.IntN(40)-20)), minSpeed, minSpeed*2))
+func vec(v rl.Vector2) Vector {
+	return Vector{X: float64(v.X), Y: float64(v.Y)}
+}
+
+func resolveCollisionWithMass(s1, s2 *Snake, dx, dy, distance float32) {
+	// Normalize the collision vector
+	nx := dx / distance
+	ny := dy / distance
+
+	// Calculate masses based on ball radius (assuming density is constant)
+	// Mass proportional to area: mass = π * r²
+	mass1 := float32(math.Pi) * s1.radius * s1.radius
+	mass2 := float32(math.Pi) * s2.radius * s2.radius
+	totalMass := mass1 + mass2
+
+	// Separate the balls to prevent overlap based on mass ratio
+	// Heavier balls move less during separation
+	overlap := s1.radius + s2.radius - distance
+	separation1 := overlap * (mass2 / totalMass)
+	separation2 := overlap * (mass1 / totalMass)
+
+	s1.pos.X -= nx * separation1
+	s1.pos.X -= ny * separation1
+	s2.pos.X += nx * separation2
+	s2.pos.X += ny * separation2
+
+	// Calculate relative velocity
+	relVelX := s2.vel.X - s1.vel.X
+	relVelY := s2.vel.Y - s1.vel.Y
+
+	// Calculate relative velocity along collision normal
+	speed := relVelX*nx + relVelY*ny
+
+	// Do not resolve if velocities are separating
+	if speed > 0 {
+		return
+	}
+
+	// Calculate restitution (bounciness) - perfect elastic collision
+	restitution := 1.0
+
+	// Calculate impulse scalar using proper mass formula
+	impulse := float32(1.0+restitution) * speed / (1.0/mass1 + 1.0/mass2)
+
+	// Apply impulse to velocities (inverse mass relationship)
+	impulseX := impulse * nx
+	impulseY := impulse * ny
+
+	s1.vel.X += impulseX / mass1
+	s1.vel.Y += impulseY / mass1
+	s2.vel.Y -= impulseX / mass2
+	s2.vel.Y -= impulseY / mass2
+}
+
+func randomColor() rl.Color {
+	return rl.NewColor(uint8(rand.IntN(255)), uint8(rand.IntN(255)), uint8(rand.IntN(255)), 255)
+}
+
+func drawFood() {
+	rl.DrawCircle(int32(food.pos.X), int32(food.pos.Y), food.radius, rl.Gold)
+}
+
+func drawSnakes() {
+	for _, s := range snakes {
+		color := s.color
+		bodyFactor := s.bodyFactor
+		c := s.chain
+		const (
+			lineThickness = 6
+		)
+
+		// skin
+		color.A = 102
+		for i, joint := range c.joints {
+			if i == 0 {
+				continue
+			}
+			size := bodyWidth(i, bodyFactor)
+			rl.DrawCircle(int32(joint.X), int32(joint.Y), size, color)
+		}
+
+		// more body
+		spine := rl.NewColor(255, 255, 255, 153)
+		for i, joint := range c.joints {
+			size := bodyWidth(i, bodyFactor) * 1.1
+			rotation := float32(c.angles[i]) * 180 / math.Pi
+			rec := rl.Rectangle{
+				X:      float32(joint.X),
+				Y:      float32(joint.Y),
+				Width:  size,
+				Height: size,
+			}
+
+			origin := rl.Vector2{X: size / 2, Y: size / 2}
+			rl.DrawRectanglePro(rec, origin, rotation, color)
+		}
+
+		// drawSnakes head
+		joint := c.joints[0]
+		b := bodyWidth(0, bodyFactor)
+		color.A = 204
+		rl.DrawCircle(int32(joint.X), int32(joint.Y), b, color)
+
+		if s.collisionTime > 0 && rl.GetTime()-s.collisionTime < CollisionTime {
+			rl.DrawCircle(int32(joint.X), int32(joint.Y), b*1.5, s.collisionColor)
+		}
+
+		// spinal column
+		for i, joint := range c.joints {
+			size := bodyWidth(i, bodyFactor) / 2.0
+			rotation := float32(c.angles[i]) * 180 / math.Pi
+			rec := rl.Rectangle{
+				X:      float32(joint.X),
+				Y:      float32(joint.Y),
+				Width:  size,
+				Height: size,
+			}
+
+			origin := rl.Vector2{X: size / 2, Y: size / 2}
+			rl.DrawRectanglePro(rec, origin, rotation, spine)
+		}
+
+		//// skin
+		//color.A = 128
+		//for i, joint := range c.joints {
+		//	b := bodyWidth(i, bodyFactor)
+		//	rl.DrawCircle(int32(joint.X), int32(joint.Y), b, color)
 		//}
-	}
 
-	s.chain.Resolve(s.pos)
+		// drawSnakes spine
+		for i := 0; i < len(c.joints)-1; i++ {
+			startJoint := c.joints[i]
+			endJoint := c.joints[i+1]
+
+			rl.DrawLineEx(vec2(startJoint), vec2(endJoint), lineThickness, spine)
+		}
+
+		rl.DrawText(s.name, int32(joint.X), int32(joint.Y), 32, rl.Black)
+	}
 }
 
-func randDir() float64 {
-	if rand.IntN(2) == 0 {
-		return rand.Float64()*0.5 - 1 // [-1, -0.5)
-	} else {
-		return rand.Float64()*0.5 + 0.5 // [0.5, 1)
-	}
-}
-
-func adjustDir(dir, change float64) float64 {
-	s := dir + rand.Float64()*(change*2) - change
-	if s > 1.0 {
-		s = 1.0
-	} else if s < -1.0 {
-		s = -1.0
+func bodyWidth(i int, bodyFactor float32) float32 {
+	var size float32 = 0.0
+	switch i {
+	case 0:
+		size = 74
+	case 1:
+		size = 80
+	default:
+		size = float32(64 - i)
 	}
 
-	return s
-}
-
-func (s *snake) draw() {
-	s.chain.Draw(s.color, s.bodyFactor)
+	return size * bodyFactor
 }
